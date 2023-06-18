c6f495e168aaf4a8ae4473b9335261695b7b79de
Fsuomins <fsuomins@student.42sp.org.br>
2023-06-18 17:04:16 -0300

    (rot_13): atualizaçao

--- level_01/rot_13/rot_13.c
+++ level_01/rot_13/rot_13.c
@@ -1,49 +1,36 @@
-#include <unistd.h>
-#include <stdio.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rot_13.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ferskys <ferskys@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/18 12:22:17 by ferskys           #+#    #+#             */
+/*   Updated: 2023/06/18 17:02:21 by ferskys          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include <stdio.h>
+# include <stdlib.h>
+#include <ctype.h>
 
 int main(int argc, char **argv)
 {
-    if (argc == 2) // Check if there is exactly one argument passed to the program
+    if (argc == 2)
     {
-        while (*argv[1]) // Iterate over each character of the argument
+        while (*argv[1])
         {
-            if ((*argv[1] >= 'a' && *argv[1] <= 'z') || (*argv[1] >= 'A' && *argv[1] <= 'Z'))
+            if (isalpha(*argv[1]))
             {
                 if ((*argv[1] >= 'a' && *argv[1] <= 'm') || (*argv[1] >= 'A' && *argv[1] <= 'M'))
-                    *argv[1] += 13; // Rotate characters 'a' to 'm' by 13 positions forward
+                    *argv[1] += 13;
                 else if ((*argv[1] >= 'n' && *argv[1] <= 'z') || (*argv[1] >= 'N' && *argv[1] <= 'Z'))
-                    *argv[1] -= 13; // Rotate characters 'n' to 'z' by 13 positions backward
+                    *argv[1] -= 13;
             }
-            printf("%c", *argv[1]); // Print the current character
-            argv[1]++; // Move to the next character
+            printf("%c", *argv[1]);
+            argv[1]++;
         }
     }
-    printf("\n"); // Print a new line character
+    printf("\n");
     return 0;
 }
-/*
-#include <unistd.h>
-#include <stdio.h>
-
-int main(int argc, char **argv)
-{
-    if (argc == 2) // Verifica se há exatamente um argumento passado para o programa
-    {
-        while (*argv[1]) // Itera sobre cada caractere do argumento
-        {
-            if ((*argv[1] >= 'a' && *argv[1] <= 'z') || (*argv[1] >= 'A' && *argv[1] <= 'Z'))
-            {
-                if ((*argv[1] >= 'a' && *argv[1] <= 'm') || (*argv[1] >= 'A' && *argv[1] <= 'M'))
-                    *argv[1] += 13; // Rotaciona os caracteres 'a' a 'm' em 13 posições para a frente
-                else if ((*argv[1] >= 'n' && *argv[1] <= 'z') || (*argv[1] >= 'N' && *argv[1] <= 'Z'))
-                    *argv[1] -= 13; // Rotaciona os caracteres 'n' a 'z' em 13 posições para trás
-            }
-            printf("%c", *argv[1]); // Imprime o caractere atual
-            argv[1]++; // Move para o próximo caractere
-        }
-    }
-    printf("\n"); // Imprime um caractere de nova linha
-    return 0;
-}
-
-*/
