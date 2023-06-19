25d552bd0bb37706f4311e5cc2a254cb977ec8cb
Fsuomins <fsuomins@student.42sp.org.br>
2023-06-19 19:05:50 -0300

    ft_itoa/2

--- level_04/ft_itoa/ft_itoa2.c
+++ level_04/ft_itoa/ft_itoa2.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*   ft_itoa2.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ferskys <ferskys@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/16 21:41:24 by ferskys           #+#    #+#             */
-/*   Updated: 2023/06/18 17:38:13 by ferskys          ###   ########.fr       */
+/*   Updated: 2023/06/18 18:14:45 by ferskys          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,15 @@ char    *ft_itoa(int nbr)
 
     if (n <= 0)
         len++;
+
     while (n)
     {
         n /= 10;
         len++;
     }
-    
-    char *result = (char *)malloc((len + 1) * (sizeof(char)));
-    
+
+    char *result = (char*)malloc((len + 1) * sizeof(char));
+
     if (result == NULL)
         return (NULL);
     
@@ -38,7 +39,7 @@ char    *ft_itoa(int nbr)
         result[0] = '0';
         return (result);
     }
-    if (nbr < 0)
+    else if (nbr < 0)
     {
         result[0] = '-';
         nbr = -nbr;
@@ -49,5 +50,5 @@ char    *ft_itoa(int nbr)
         result[--len] = nbr % 10 + '0';
         nbr /= 10;
     }
-    return (result);   
+    return (result);
 }
