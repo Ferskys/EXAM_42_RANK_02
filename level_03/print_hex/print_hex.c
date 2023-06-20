137eaa26ca90800e4b3f7c089534f00c142ddefd
Fernanda Suominsky <fsuomins@c3r1p3.42sp.org.br>
2023-06-20 17:31:21 -0300

    feat(print_hex): Refactor.

--- level_03/print_hex/print_hex.c
+++ level_03/print_hex/print_hex.c
@@ -3,28 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   print_hex.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ferskys <ferskys@student.42.fr>            +#+  +:+       +#+        */
+/*   By: fsuomins <fsuomins@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/16 18:29:31 by ferskys           #+#    #+#             */
-/*   Updated: 2023/06/16 18:33:37 by ferskys          ###   ########.fr       */
+/*   Updated: 2023/06/20 17:30:16 by fsuomins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
+#define HEX "0123456789abcdef"
 
 void print_hex(int num)
 {
     if (num > 0)
     {
         print_hex(num / 16);
-
         int rest = num % 16;
-
-        if (rest < 10)
-            printf("%c", rest + '0');
-        else
-            printf("%c", rest - 10 + 'a');
+		printf("%c", HEX[rest]);
     }
 }
 
